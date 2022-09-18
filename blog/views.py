d7f7771e966229679e65a057f1780be4e06aa819
Anu Sethi <anu1301@hotmail.co.uk>
2022-09-18 19:04:04 +0000

    Register models and creae post list views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+"""
+Class based views used to enable reuseable code,
+allowing one view to inherit from another.
+The use of django built-in features have been used.
+"""
+class PostList(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+    paginate_by = 6
