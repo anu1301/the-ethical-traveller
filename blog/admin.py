5b90bd4efc64a6f41242453c9e10a571ab8c3cf5
Anu Sethi <anu1301@hotmail.co.uk>
2022-11-03 18:51:42 +0000

    Add comment to code

--- blog/admin.py
+++ blog/admin.py
@@ -2,14 +2,16 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-
 """ 
-Blog Admin config. Models registered here 
+Admin config - Models registered here 
 """
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+    """
+    Fields for Post Model in admin panel
+    """
     list_display = ('title', 'slug', 'status', 'created_on', 'pk')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
@@ -19,11 +21,16 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-
+    """
+    Fields for Comment Model in admin panel
+    """
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ['name', 'email', 'body']
     actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
+        """
+        Enables admin to approve user comments
+        """
         queryset.update(approved=True)
