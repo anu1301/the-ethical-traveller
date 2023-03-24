649ad58d093d8d18d850937f8a4c26ad46a611d1
Anu Sethi <anu1301@hotmail.co.uk>
2023-03-24 18:55:00 +0000

    Created holiday detail template, accessible from holiday list page

--- holiday/urls.py
+++ holiday/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('holiday/', views.ProductList.as_view(), name='product_list'),
-]
+    path('holiday/<int:id>/', views.ProductDetail.as_view(), name='product_detail'),
+   ]
--- holiday/views.py
+++ holiday/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Product
 
 
@@ -7,3 +7,23 @@ class ProductList(generic.ListView):
     model = Product
     template_name = 'holiday/holiday.html'
     paginate_by = 3
+
+
+class ProductDetail(View):
+    """
+    Displays detailed page view of individual holiday.
+    """
+
+    def get(self, request, id, *args, **kwargs):
+        """
+        Retrieves and displays holiday details.
+        """
+        # queryset = Product.objects.all()
+        product = get_object_or_404(Product, pk=id)
+
+        return render(
+            request,
+            "holiday/holiday_detail.html",
+            {"product": product}
+        )
+
